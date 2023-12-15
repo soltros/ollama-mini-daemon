b0ca6fb6be4e8a534026267261d1738f98cb0719
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-15 16:02:10 -0500

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -1,65 +1,31 @@
 #!/usr/bin/env python3
 
-import socket
 import subprocess
 import threading
-import os
-import sys
-import signal
 
-def run_command():
-    process = subprocess.Popen(["ollama", "serve"])
-    return process
+def read_output(process):
+    while True:
+        output = process.stdout.readline()
+        if output == '':
+            break
+        print(output.strip())
 
-def handle_client(client_socket, command_process):
-    with client_socket:
-        try:
-            while True:
-                data = client_socket.recv(1024).decode()
-                if not data:
-                    break
-                if data.lower() == 'shutdown':
-                    command_process.terminate()
-                    break
-        except ConnectionResetError:
-            pass  # Handle client disconnecting
+def run_npm():
+    process = subprocess.Popen(['npm', 'run', 'dev'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
-def start_server(command_process):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind(('localhost', 5000))
-        server.listen()
+    # Start a thread to read and print output
+    output_thread = threading.Thread(target=read_output, args=(process,))
+    output_thread.start()
 
-        while True:
-            client_socket, addr = server.accept()
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, command_process))
-            client_thread.start()
+    # Wait for a key press to terminate the program
+    input("Press enter to stop the program: ")
 
-def daemonize():
-    if os.fork() > 0:
-        sys.exit(0)
+    # Terminate the npm process
+    process.kill()
+    process.wait()  # Wait for the process to terminate completely
 
-    os.chdir('/')
-    os.setsid()
-    os.umask(0)
-
-    if os.fork() > 0:
-        sys.exit(0)
-
-    sys.stdout.flush()
-    sys.stderr.flush()
-    with open('/dev/null', 'rb') as f:
-        os.dup2(f.fileno(), sys.stdin.fileno())
-    with open('/dev/null', 'ab') as f:
-        os.dup2(f.fileno(), sys.stdout.fileno())
-        os.dup2(f.fileno(), sys.stderr.fileno())
-
-    signal.signal(signal.SIGTERM, lambda signum, frame: sys.exit(0))
-
-def main():
-    daemonize()
-
-    process = run_command()
-    start_server(process)
+    # Wait for the output thread to finish
+    output_thread.join()
 
 if __name__ == "__main__":
-    main()
+    run_npm()
